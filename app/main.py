86501f09735eea737c0c0132d2acc7eb102053e7
Velkog <60084050+velkog@users.noreply.github.com>
2023-04-03 02:53:56 -0700

    Add initial command for KoggersBot

---
+++ app/main.py
@@ -0,0 +1,24 @@
+# This example requires the 'message_content' intent.
+
+import discord
+from os import getenv
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "KoggersBot" in message.content:
+        await message.channel.send("https://tenor.com/view/breaking-bad-bb-jesse-jesse-yeah-bitch-yeah-bitch-gif-15852209")
+
+DISCORD_BOT_TOKEN = getenv("DISCORD_BOT_TOKEN")
+client.run(DISCORD_BOT_TOKEN)
